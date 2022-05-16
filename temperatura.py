67ef0b0ee936dd80b42255922bf2ce4beef2bb59
rodrigoas93 <105251829+rodrigoas93@users.noreply.github.com>
2022-05-16 00:49:11 -0500

    notas de temperatura
    Hacer un programa que te pida a cuantos grados centigrados estamos
    Dependiendo el dato ingresado realizar las siguientes validaciones
    
    si es mas de 35 grados mostrar: "ta juerte la calor"
    
    si esta entre 25 a 30 grados mostrar: "se antoja un pozol"
    
    si la temperatura es menor a 25 grados mostrar: "es hora de sacar el sueter cucarachiento"
    
    
    utilizar el menor lineas de codigo posible
    
    se evaluara:
    legibilidad de codigo
    
    
    "Simple es mejor que complejo"

---
+++ temperatura.py
@@ -0,0 +1,8 @@
+temperatura = int(input("¿Cual es la temperatura el dia de hoy?\n" ))
+
+if temperatura >35:
+    print("ta juerte la calor")
+elif temperatura <36 and temperatura >25:
+    print("se antoja un pozol")
+else:
+    print("es hora de sacar el sueter cucarachiento")
